24ab8eafdfc72ee3997cd4a56527d9d3224b8028
Jay <jaypranil1503@gmail.com>
2026-05-28 17:38:18 +0530

    Done with todo list

--- toList/src/App.jsx
+++ toList/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TodoProvider } from './Context/Index'
+import TodoForm from './Components/TodoForm'
+import TodoItem from './Components/Todoitem'
 
 
 function App() {
@@ -10,6 +12,31 @@ function App() {
     setTodos((prev) => [{id: Date.now(),...todo},...prev])
   }
   
+  const updateTodo = (id,todo) =>{
+    setTodos((prev ) => prev.map((prevTodo) => (prevTodo.id === id ? todo :prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed}:
+  prevTodo))
+  }
+
+  useEffect(()=> {
+    const todos =JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length>0)
+    {
+      setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
 
   return (
     <TodoProvider value = {{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
@@ -17,10 +44,16 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {/* Todo form goes here */}
+                        <TodoForm/> 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo ) => (
+                          <div key={todo.id} className='w-full' >
+                            <TodoItem todo = {todo} />
+                          </div>
+                        ))}
                     </div>
                 </div>
     </div>
