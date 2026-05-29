d0b8ccd23778c1a953d655529888ca6ff09feeb0
Jay <jaypranil1503@gmail.com>
2026-05-29 16:51:42 +0530

    started the mega project(did work related to appwrite)

---
+++ Mega-Blog/src/appwrite/auth.jsx
@@ -0,0 +1,9 @@
+import conf from "../conf/Conf";
+
+import { Client, Account,ID } from "appwrite";
+
+export class AuthService {}
+
+const authservice = new AuthService();
+
+export default AuthService;
