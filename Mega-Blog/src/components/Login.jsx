f3e87909a7bfdb36ea3059806231ed2e9900a6f9
Jay <jaypranil1503@gmail.com>
2026-06-05 18:59:42 +0530

    Removed unnecessary things

--- Mega-Blog/src/components/Login.jsx
+++ Mega-Blog/src/components/Login.jsx
@@ -40,7 +40,7 @@ function Login(){
                 Sign in to your account
             </h2>
             <p className="mt-2 text-center text-base text-black/60">
-                        Don&apos;t have any account?&nbsp;
+                        Dont have any account?
                         <Link
                             to="/signup"
                             className="font-medium text-primary transition-all duration-200 hover:underline"
